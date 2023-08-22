d67efec506b95f18c2d327ff1083aa862ed5276b
Jonas Nilsson <jonas.nilsson@disruptivei.com>
2023-08-22 02:59:57 +0200

    Added test for folder sturcture support

--- VisualArt.Media.IntegrationTest/TestMediaStoreApi.cs
+++ VisualArt.Media.IntegrationTest/TestMediaStoreApi.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http.Json;
 using VisualArt.Media.Dto;
@@ -53,6 +52,22 @@ namespace VisualArt.Media.IntegrationTest
             Assert.Equal("application/json; charset=utf-8", response!.Content!.Headers!.ContentType!.ToString());
         }
 
+        [Theory]
+        [InlineData("/api/media/metadata/<")]
+        [InlineData("/api/media/metadata/>")]
+        [InlineData("/api/media/metadata/|")]
+        [InlineData("/api/media/metadata/:")]
+        public async Task Get_EndpointsReturnBadRequestWhenCalledWithInvalidPath(string url)
+        {
+            // Arrange
+            var client = CreateInjectedClient();
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
         [Fact]
         public async Task GivenOneFile_SavesOneFile()
         {
@@ -75,6 +90,38 @@ namespace VisualArt.Media.IntegrationTest
         }
 
         [Fact]
+        public async Task TryingToSaveFile20LevelsDeep_ReturnsBadRequest()
+        {
+            // Arrange
+            var client = CreateInjectedClient();
+            var content = new MultipartFormDataContent();
+            var file = FileUtil.OpenRead($"StoreFiles/{_samples[0].name}");
+            content.Add(new StreamContent(file), "files", _samples[0].name);
+
+            // Act
+            var response = await client.PostAsync("/api/media/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20", content);
+
+            // Assert
+            Assert.Equal( System.Net.HttpStatusCode.BadRequest, response.StatusCode );
+        }
+
+        [Fact]
+        public async Task TryingToListFiles20LevelsDeep_ReturnsBadRequest()
+        {
+            // Arrange
+            var client = CreateInjectedClient();
+            var content = new MultipartFormDataContent();
+            var file = FileUtil.OpenRead($"StoreFiles/{_samples[0].name}");
+            content.Add(new StreamContent(file), "files", _samples[0].name);
+
+            // Act
+            var response = await client.GetAsync("/api/media/metadata/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20");
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
         public async Task GivenFiveFiles_SavesFiveFiles()
         {
             // Arrange
@@ -91,7 +138,7 @@ namespace VisualArt.Media.IntegrationTest
             // Assert
             response.EnsureSuccessStatusCode();
             Assert.Equal("application/json; charset=utf-8", response!.Content!.Headers!.ContentType!.ToString());
-            var files = await response.Content.ReadFromJsonAsync<List<FileMetadata>>() ?? throw new Exception("Null response"); 
+            var files = await response.Content.ReadFromJsonAsync<List<FileMetadata>>() ?? throw new Exception("Null response");
             Assert.Equal(_samples.Count, files?.Count);
 
             foreach (var file in files!)
@@ -135,6 +182,84 @@ namespace VisualArt.Media.IntegrationTest
             }
         }
 
+        [Fact]
+        public async Task SavesFiveFiles_ThreeLevelsDeep_ListMetadataReturnsAllFiles()
+        {
+            //https://www.istockphoto.com/en/collaboration/boards/PRk4F0mZ_E6z_qcKsC2qSg
+
+            // Arrange
+            var client = CreateInjectedClient();
+            var content = new MultipartFormDataContent();
+            foreach (var file in _samples)
+            {
+                content.Add(new StreamContent(FileUtil.OpenRead($"StoreFiles/{file.name}")), "files", file.name);
+            }
+
+            // Act
+            var response = await client.PostAsync("/api/media/level1/level2/level3", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8", response!.Content!.Headers!.ContentType!.ToString());
+            var files = await response.Content.ReadFromJsonAsync<List<FileMetadata>>();
+            Assert.Equal(_samples.Count, files?.Count);
+
+            foreach (var file in files!)
+            {
+                Assert.NotEqual(-1, _samples.IndexOf((file.Name, file.Length)));
+            }
+
+            var fileMetadata = await client.GetFromJsonAsync<List<FileMetadata>>("/api/media/metadata/level1/level2/level3");
+            Assert.Equal(_samples.Count, fileMetadata?.Count);
+            foreach (var file in fileMetadata!)
+            {
+                Assert.NotEqual(-1, _samples.IndexOf((file.Name, file.Length)));
+            }
+        }
+
+        [Fact]
+        public async Task SavesFiveFiles_AtAllLevels_ThreeLevelsDeep_ListMetadataReturnsAllFiles()
+        {
+            //https://www.istockphoto.com/en/collaboration/boards/PRk4F0mZ_E6z_qcKsC2qSg
+
+            // Arrange
+            var client = CreateInjectedClient();
+            var content = new MultipartFormDataContent();
+            foreach (var file in _samples)
+            {
+                content.Add(new StreamContent(FileUtil.OpenRead($"StoreFiles/{file.name}")), "files", file.name);
+            }
+
+            var folders = new List<string> { "", "/level1", "/level1/level2", "/level1/level2/level3" };
+            // Act
+            foreach (var folder in folders)
+            {
+                var response = await client.PostAsync($"/api/media{folder}", content);
+
+                // Assert
+                response.EnsureSuccessStatusCode();
+                Assert.Equal("application/json; charset=utf-8", response!.Content!.Headers!.ContentType!.ToString());
+                var files = await response.Content.ReadFromJsonAsync<List<FileMetadata>>();
+                Assert.Equal(_samples.Count, files?.Count);
+
+                foreach (var file in files!)
+                {
+                    Assert.NotEqual(-1, _samples.IndexOf((file.Name, file.Length)));
+                }
+
+                var fileMetadata = await client.GetFromJsonAsync<List<FileMetadata>>($"/api/media/metadata{folder}");
+                var fileCount = 0;
+                foreach (var file in fileMetadata!)
+                {
+                    if (file.Folder == false)
+                    {
+                        fileCount++;
+                        Assert.NotEqual(-1, _samples.IndexOf((file.Name, file.Length)));
+                    }
+                }
+                Assert.Equal(_samples.Count, fileCount);
+            }
+        }
         [Fact]
         public async Task GivenOneFile_OverMaxSize_IgnoresSaving()
         {
